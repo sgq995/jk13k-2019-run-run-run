01225640040b346a80a76179261b9653fe6fa8c3
Sebastián <sgq995@gmail.com>
2019-08-19 17:52:29 -0500

    chore: NodeJS and Gulp.js config added

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const webpack = require('webpack-stream');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const zip = require('gulp-zip');
+
+const js = 'src/index.js';
+const html = 'src/index.html';
+const images = 'assets/*.png';
+
+const outputDir = 'public/';
+
+const jsOutputFilename = 'game.js';
+const htmlOutputFilename = 'index.html';
+
+function jsBuild(cb) {
+    return gulp.src(js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(webpack({
+            mode: 'production',
+            output: {
+                filename: jsOutputFilename
+            }
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(outputDir));
+}
+
+function jsBuildDev(cb) {
+    return gulp.src(js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: jsOutputFilename
+            }
+        }))
+        .pipe(gulp.dest(outputDir));
+}
+
+function htmlMinify(cb) {
+    return gulp.src(html)
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest(outputDir));
+}
+
+function htmlMinifyDev(cb) {
+    return gulp.src(html)
+        .pipe(htmlmin({
+            removeComments: true
+        }))
+        .pipe(gulp.dest(outputDir));
+}
+
+const build = gulp.parallel(jsBuild, htmlMinify);
+
+const buildDev = gulp.parallel(jsBuildDev, htmlMinifyDev);
+
+function watch(cb) {
+    gulp.watch(js, jsBuild);
+    gulp.watch(html, htmlMinify);
+}
+
+const publish = gulp.series(gulp.parallel(jsBuild, htmlMinify), (cb) => {
+    return gulp.src([`${outputDir}${jsOutputFilename}`, `${outputDir}${htmlOutputFilename}`])
+        .pipe(zip('release.zip'))
+        .pipe(gulp.dest('build'));
+});
+
+exports.default = build;
+exports.build = build;
+exports.buildDev = buildDev;
+exports.watch = watch;
+exports.publish = publish;
