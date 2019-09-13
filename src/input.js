e6cd60495adfc27f84bf97f6dd487c770b926db9
Sebastián <sgq995@gmail.com>
2019-09-13 07:53:16 -0500

    feat: Start key detection was added to input.js

--- src/input.js
+++ src/input.js
@@ -1,6 +1,7 @@
 import { Rect } from "./rect";
 
 const Keyboard = {
+    VK_START: false,
     VK_PAUSE: false,
 
     VK_UP: false,
@@ -10,6 +11,10 @@ const Keyboard = {
 };
 const handler = (e, v) => {
     switch (e.keyCode) {
+        case 13:
+            Keyboard.VK_START = v;
+            break;
+        
         case 27:
         case 80:
             Keyboard.VK_PAUSE = v;
@@ -47,6 +52,10 @@ export class Input {
     constructor() {
         this.delta = new Rect();
 
+        this.startPressed = false;
+        this.startKeyCurrentState = Keyboard.VK_START;
+        this.startKeyPreviousState = this.startKeyCurrentState;
+
         this.pausePressed = false;
         this.pauseKeyCurrentState = Keyboard.VK_PAUSE;
         this.pauseKeyPreviousState = this.pauseKeyCurrentState;
@@ -58,6 +67,12 @@ export class Input {
         return pausePressed;
     }
 
+    get isStartPressed() {
+        let startPressed = this.startPressed;
+        this.startPressed = false;
+        return startPressed;
+    }
+
     handle(delta) {
         this.pauseKeyCurrentState = Keyboard.VK_PAUSE;
         if (this.pauseKeyCurrentState === false && this.pauseKeyPreviousState === true) {
@@ -65,6 +80,12 @@ export class Input {
         }
         this.pauseKeyPreviousState = this.pauseKeyCurrentState;
         
+        this.startKeyCurrentState = Keyboard.VK_START;
+        if (this.startKeyCurrentState === false && this.startKeyPreviousState === true) {
+            this.startPressed = true;
+        }
+        this.startKeyPreviousState = this.startKeyCurrentState;
+
         if (Keyboard.VK_LEFT) {
             this.delta.x = -delta * 160;
         } else if (Keyboard.VK_RIGHT) {
