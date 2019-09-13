4fa8a884004cf7dee09538e69d1ba2beb93c8311
Sebastián <sgq995@gmail.com>
2019-09-12 22:35:44 -0500

    feat: Animation was added to Runners
    feat: Color fade was added in order to detect collisions graphically

--- src/runner.js
+++ src/runner.js
@@ -10,21 +10,27 @@ const DEFAULT_RUNNER_Y      = 360;
 const DEFAULT_RUNNER_WIDTH  = 120;
 const DEFAULT_RUNNER_HEIGHT = 256;
 
-const DEFAULT_ANIM_TIME    = 200;
+const DEFAULT_ANIM_TIME    = 150;
 const DEFAULT_RUNNER_SPEED = 30;
+
+const RUNNER_STATIC_STATE = 0;
+const RUNNER_LEFT_MOVE_STATE = 1;
+const RUNNER_RIGHT_MOVE_STATE = 2;
+
+function clamp(value, min, max) {
+    return Math.max(min, Math.min(value, max));
+}
 
 export class Runner {
     constructor(clock, rect=Rect.from({x: DEFAULT_RUNNER_X, y: DEFAULT_RUNNER_Y, width: DEFAULT_RUNNER_WIDTH, height: DEFAULT_RUNNER_HEIGHT}), speed=DEFAULT_RUNNER_SPEED) {
         this.spriteRect = rect;
 
-        this.staticSprite = new Sprite(Runner.buildRunnerImage(), this.spriteRect);
-        this.leftMoveSprite = new Sprite(Runner.buildRunnerImage(), this.spriteRect);
-        this.rightMoveSprite = new Sprite(Runner.buildRunnerImage(), this.spriteRect);
+        this.staticSprite = new Sprite(Runner.buildRunnerImage('#0f0', RUNNER_STATIC_STATE), this.spriteRect);
+        this.leftMoveSprite = new Sprite(Runner.buildRunnerImage('#0f0', RUNNER_LEFT_MOVE_STATE), this.spriteRect);
+        this.rightMoveSprite = new Sprite(Runner.buildRunnerImage('#0f0', RUNNER_RIGHT_MOVE_STATE), this.spriteRect);
         this.sprite = this.staticSprite;
 
         this.speed = speed;
-
-        // this.spriteRect.width = this.depth * (DEFAULT_RUNNER_WIDTH - MIN_RUNNER_WIDTH) / MAX_DEPTH + MIN_RUNNER_WIDTH;
 
         let dx = this.spriteRect.width / 4;
         let dy = this.spriteRect.height / 4;
@@ -35,12 +41,71 @@ export class Runner {
             this.spriteRect.bottom - dy);
         
         this.animTimer = new Timer(clock, DEFAULT_ANIM_TIME * DEFAULT_RUNNER_SPEED / speed);
-        this.animSpeed = -speed;
-
-        console.log(this);
+        this.anim = [RUNNER_STATIC_STATE, RUNNER_LEFT_MOVE_STATE, RUNNER_STATIC_STATE, RUNNER_RIGHT_MOVE_STATE];
+        this.animIdx = 0;
     }
 
-    static buildRunnerImage(color='#0f0') {
+    static buildHead(context, width, height) {
+        context.beginPath();
+        context.arc(width / 2, height / 8, height / 8, 0, 2 * Math.PI);
+        context.fill();
+    }
+
+    static buildTrunkAndArms(context, width, height, state=RUNNER_STATIC_STATE) {
+        context.beginPath();
+        context.moveTo(width / 2, height / 4);
+        context.lineTo(width / 8, height / 4);
+        context.arcTo(0, height / 4, 0, height / 4 + height / 8, height / 8);
+        
+        if (state === RUNNER_LEFT_MOVE_STATE) {
+            context.lineTo(0, height / 2);
+            context.lineTo(width / 4, height / 2);
+            context.lineTo(width / 4, 3 * height / 5);
+        } else {
+            context.lineTo(0, 3 * height / 5);
+        }
+        
+        if (state === RUNNER_RIGHT_MOVE_STATE) {
+            context.lineTo(3 * width / 4, 3 * height / 5);
+            context.lineTo(3 * width / 4, height / 2);
+            context.lineTo(width, height / 2);
+        } else {
+            context.lineTo(width, 3 * height / 5);
+            context.lineTo(width, height / 4 + height / 8);
+        }
+        
+        context.arcTo(width, height / 4, width - width / 8, height / 4, height / 8);
+        context.closePath();
+        context.fill();
+    }
+
+    static buildLegs(context, width, height, state=RUNNER_STATIC_STATE) {
+        context.beginPath();
+        context.moveTo(width / 4, height / 2);
+
+        if (state === RUNNER_RIGHT_MOVE_STATE) {
+            context.lineTo(width / 4, 3 * height / 4);
+            context.lineTo(width / 2, 3 * height / 4);
+            context.lineTo(width / 2, height);
+        } else {
+            context.lineTo(width / 4, height);
+        }
+        
+        if (state === RUNNER_LEFT_MOVE_STATE) {
+            context.lineTo(width / 2, height);
+            context.lineTo(width / 2, 3 * height / 4);
+            context.lineTo(3 * width / 4, 3 * height / 4);
+        } else {
+            context.lineTo(3 * width / 4, height);
+        }
+
+        context.lineTo(3 * width / 4, height / 2);
+
+        context.closePath();
+        context.fill();
+    }
+
+    static buildRunnerImage(color='#0f0', state=RUNNER_STATIC_STATE) {
         const canvas = document.createElement('canvas');
         canvas.width = 120;
         canvas.height = 256;
@@ -48,31 +113,9 @@ export class Runner {
         const context = canvas.getContext('2d');
         context.fillStyle = color;
 
-        // Head
-        context.beginPath();
-        context.arc(canvas.width / 2, canvas.height / 8, canvas.height / 8, 0, 2 * Math.PI);
-        context.fill();
-
-        // Trunk
-        context.beginPath();
-        context.moveTo(canvas.width / 2, canvas.height / 4);
-        context.lineTo(canvas.width / 8, canvas.height / 4);
-        context.arcTo(0, canvas.height / 4, 0, canvas.height / 4 + canvas.height / 8, canvas.height / 8);
-        context.lineTo(0, 3 * canvas.height / 5);
-        context.lineTo(canvas.width, 3 * canvas.height / 5);
-        context.lineTo(canvas.width, canvas.height / 4 + canvas.height / 8);
-        context.arcTo(canvas.width, canvas.height / 4, canvas.width - canvas.width / 8, canvas.height / 4, canvas.height / 8);
-        context.closePath();
-        context.fill();
-
-        // Legs
-        context.beginPath();
-        context.moveTo(canvas.width / 4, canvas.height / 2);
-        context.lineTo(canvas.width / 4, canvas.height);
-        context.lineTo(3 * canvas.width / 4, canvas.height);
-        context.lineTo(3 * canvas.width / 4, canvas.height / 2);
-        context.closePath();
-        context.fill();
+        Runner.buildHead(context, canvas.width, canvas.height, state);
+        Runner.buildTrunkAndArms(context, canvas.width, canvas.height, state);
+        Runner.buildLegs(context, canvas.width, canvas.height, state);
 
         return canvas;
     }
@@ -134,21 +177,26 @@ export class Runner {
 
         if (this.animTimer.timeout()) {
             this.animTimer.tick();
-            this.animSpeed *= -1;
+            
+            this.animIdx = (this.animIdx + 1) % this.anim.length;
+            if (this.anim[this.animIdx] === RUNNER_STATIC_STATE) {
+                this.sprite = this.staticSprite;
+            } else if (this.anim[this.animIdx] === RUNNER_LEFT_MOVE_STATE) {
+                this.sprite = this.leftMoveSprite;
+            } else if (this.anim[this.animIdx] === RUNNER_RIGHT_MOVE_STATE) {
+                this.sprite = this.rightMoveSprite;
+            }
         }
-
-        //this.image.context.fillRect();
-        // this.rect.y += deltaTime * this.animSpeed;
     }
 
     draw(context) {
         this.sprite.draw(context);
         
-        context.strokeRect(
+        /*context.strokeRect(
             this.collisionRect.x, 
             this.collisionRect.y,
             this.collisionRect.width,
-            this.collisionRect.height);
+            this.collisionRect.height);*/
     }
 }
 
@@ -158,20 +206,45 @@ export class AutonomousRunner extends Runner {
 
         this.color = '#00f';
         this.targetSpeed = targetSpeed;
+
+        this.isCollided = false;
+        this.collisionFactor = 0.0;
     }
 
     set color(value) {
-        this.staticSprite.image = Runner.buildRunnerImage(value);
-        this.leftMoveSprite.image = Runner.buildRunnerImage(value);
-        this.rightMoveSprite.image = Runner.buildRunnerImage(value);
+        this.staticSprite.image = Runner.buildRunnerImage(value, RUNNER_STATIC_STATE);
+        this.leftMoveSprite.image = Runner.buildRunnerImage(value, RUNNER_LEFT_MOVE_STATE);
+        this.rightMoveSprite.image = Runner.buildRunnerImage(value, RUNNER_RIGHT_MOVE_STATE);
     }
     
+    updateColor() {
+        let r = clamp(Math.round(this.collisionFactor * 255), 0, 255);
+        let b = clamp(Math.round((1.0 - this.collisionFactor) * 255), 0, 255);
+
+        r = r.toString(16);
+        r = r.length < 2 ? '0' + r : r;
+
+        b = b.toString(16);
+        b = b.length < 2 ? '0' + b : b;
+
+        this.color = `#${r}00${b}`;
+    }
+
     update(deltaTime) {
         super.update(deltaTime);
 
         let speed = this.targetSpeed - this.speed;
-        let deltaY = deltaTime * speed;
         
+        let collisionFactorDelta = deltaTime * speed / this.targetSpeed;
+        if (this.isCollided && this.collisionFactor < 1.0) {
+            this.collisionFactor = Math.min(this.collisionFactor + collisionFactorDelta, 1.0);
+            this.updateColor();
+        } else if (this.collisionFactor > 0.0) {
+            this.collisionFactor = Math.max(this.collisionFactor - collisionFactorDelta, 0.0);
+            this.updateColor();
+        }
+
+        let deltaY = deltaTime * speed;
         this.y += deltaY;
         
         let newHeight = this.y / DEFAULT_RUNNER_Y * DEFAULT_RUNNER_HEIGHT;
@@ -184,6 +257,9 @@ export class AutonomousRunner extends Runner {
             this.width = newWidth;    
         } else if (this.x < threshold) {
             this.x += this.width - newWidth;
+            this.width = newWidth;
+        } else {
+            this.x -= Math.abs(this.width - newWidth) / 2;
             this.width = newWidth;
         }
     }
